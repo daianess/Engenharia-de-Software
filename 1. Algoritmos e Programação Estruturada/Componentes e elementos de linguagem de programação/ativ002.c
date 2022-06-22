d87cefb70aeeb22cc4da52c9ad8c703eb76cb8a0
Daiane da S. Santos <daianesadm@outlook.com>
2022-06-22 05:22:31 -0300

    Unidade 1

--- 1. Algoritmos e Programação Estruturada/Componentes e elementos de linguagem de programação/ativ002.c
+++ 1. Algoritmos e Programação Estruturada/Componentes e elementos de linguagem de programação/ativ002.c
@@ -7,10 +7,17 @@ int main()
   char genero = 'F';
   float altura = 1.63;
 
-  printf("\n Idade: %d", idade);
-  printf("\n Salário: %f", salario);
+  printf("\n Idade: %d", idade); 
+  printf("\n Salário: %f", salario); 
   printf("\n Desconto: %f", porcentagem_desconto);
-  printf("\n Gênero: %c", genero);
-  printf("\n Altura: %.3f", altura);
+  printf("\n Gênero: %c", genero); 
+  printf("\n Altura: %.3f", altura); 
   return 0;
+
+  // \n = gera uma quebra de linha
+  // %d = tipo inteiro
+  // %f = pontos flutuantes
+  // %c = caractere
+  // .3 = resultado será impresso com três casas decimais
+
 }
